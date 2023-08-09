0d6e38f9b3319f95b413d8805bdafd76cbb9d55b
Taras Kovalyk <tarik2454@gmail.com>
2023-08-09 21:50:23 +0300

    Adds refactoring of class components to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { StyledWrapper, StyledSmollTitle } from './App.styled';
 import { StyledMain } from 'style/GlobalStyle';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleButtonClick = name => {
+    switch (name) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  handleButtonClick = name => {
-    this.setState(prev => ({ [name]: prev[name] + 1 }));
-  };
+  const total = good + neutral + bad;
+  const positiveFeedbackPercentage =
+    total === 0 ? 0 : ((good / total) * 100).toFixed(2);
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
+  return (
+    <StyledMain>
+      <StyledWrapper>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={handleButtonClick}
+          />
+        </Section>
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    if (total === 0) return 0;
-    return (good / total) * 100;
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positiveFeedbackPercentage =
-      this.countPositiveFeedbackPercentage().toFixed(2);
-
-    return (
-      <StyledMain>
-        <StyledWrapper>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={Object.keys(this.state)}
-              onLeaveFeedback={this.handleButtonClick}
+        {total > 0 ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positiveFeedbackPercentage={positiveFeedbackPercentage}
             />
           </Section>
-
-          {total > 0 ? (
-            <Section title="Statistics">
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={total}
-                positiveFeedbackPercentage={positiveFeedbackPercentage}
-              />
-            </Section>
-          ) : (
-            <StyledSmollTitle>There is no feedback</StyledSmollTitle>
-          )}
-        </StyledWrapper>
-      </StyledMain>
-    );
-  }
-}
+        ) : (
+          <StyledSmollTitle>There is no feedback</StyledSmollTitle>
+        )}
+      </StyledWrapper>
+    </StyledMain>
+  );
+};
